578de7a4fa01e238b9896854450c9e2e07cbe5c5
Vinicius <viniciusjb2013@hotmail.com>
2019-08-02 20:31:51 -0300

    Exercício 2 - TypeScript

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 5;
+var nota3 = 7;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média é " + media);
+if (media >= 7) {
+    console.log("Aprovado");
+}
+else {
+    console.log("Reprovado");
+}
---
+++ exercicio1.ts
@@ -0,0 +1,14 @@
+let nota1: number = 10
+let nota2: number = 5
+let nota3: number = 7
+
+let media: number = (nota1 + nota2 + nota3) / 3
+
+console.log("A média é " + media)
+
+if(media >= 7){
+    console.log("Aprovado")
+}
+else{
+    console.log("Reprovado")
+}
