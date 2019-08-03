0030a361c3ed288fe5649cec1c1cff5e9d493e22
Vinicius <viniciusjb2013@hotmail.com>
2019-08-02 21:49:50 -0300

    Exercicío 2 - Aula 2

---
+++ dist/exercicio2.js
@@ -0,0 +1,9 @@
+var competidores = 10;
+var quantidadeFolhas = 90;
+var folhaCompetidor = 10;
+if (competidores * folhaCompetidor > quantidadeFolhas) {
+    console.log("NÂO");
+}
+else {
+    console.log("SIM");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,10 @@
+let competidores: number = 10
+let quantidadeFolhas: number = 90
+let folhaCompetidor: number = 10
+
+if(competidores * folhaCompetidor > quantidadeFolhas){
+    console.log("NÂO")
+}
+else {
+    console.log("SIM")
+}
